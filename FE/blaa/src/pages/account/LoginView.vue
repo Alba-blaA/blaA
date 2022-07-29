047df7a7efe001f260185f39c2df3bacfc53c893
asdf134652 <asdf134652@gmail.com>
2022-07-29 21:08:48 +0900

    FEAT: skeleton code finish

---
+++ FE/blaa/src/pages/account/LoginView.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>여기는 로그인 페이지입니다!</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ FE/blaa/src/pages/review/ReviewView.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>여기는 리뷰 페이지입니다!</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
