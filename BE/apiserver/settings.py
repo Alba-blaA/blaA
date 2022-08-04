869d1a2ee5ffa255b636fc959123254ad93c440d
asdf134652 <asdf134652@gmail.com>
2022-08-04 13:12:52 +0900

    [S07P12B209-184] FEAT: Crew Model

--- BE/apiserver/settings.py
+++ BE/apiserver/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'categorys',
     'reviews',
     'stories',
+    'crews',
+
+    #3rd-party
     'django_extensions',
     'corsheaders',
     'django_filters',
@@ -147,7 +150,7 @@ REST_FRAMEWORK = {
     ],
     # 'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE':3
+    'PAGE_SIZE':10
 }
 
 JWT_AUTH = { 
--- BE/apiserver/urls.py
+++ BE/apiserver/urls.py
@@ -26,7 +26,7 @@ schema_view = get_schema_view(
    openapi.Info(
     title="blaA API",
     default_version='v1',
-    description="Test description",
+    description="BlaA API Test description",
     terms_of_service="https://www.google.com/policies/terms/",
     contact=openapi.Contact(email="contact@snippets.local"),
     license=openapi.License(name="BSD License"),
