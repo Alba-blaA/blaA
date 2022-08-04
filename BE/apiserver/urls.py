a77aa01ddbeb86e2ef208df70f99ddeffe4c60f6
asdf134652 <asdf134652@gmail.com>
2022-08-04 12:42:36 +0900

    [S07P12B209-216] Media/ url router

--- BE/apiserver/urls.py
+++ BE/apiserver/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -50,4 +51,4 @@ urlpatterns = [
     # path('swagger(?P<format>\.json|\.yaml)$/', schema_view.without_ui(cache_timeout=0), name='swagger'),
     path('api/v1/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/v1/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
