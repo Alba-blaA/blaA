2aefa4be4a171452bd2de11ca023d18bfe1d1cbb
Sohee Cheon <csh5156@gmail.com>
2022-08-05 13:52:47 +0900

    FEAT: map api search in progress

--- FE/blaa/src/components/review/ReviewMap.vue
+++ FE/blaa/src/components/review/ReviewMap.vue
@@ -6,11 +6,9 @@
           <p>장소를 검색하세요 <span  @click="$emit('close-modal')" style="cursor:pointer"> 닫기</span></p>
           <input type="text" v-model="searchWord" @keyup.enter="searchStore">
         </div>
-        <hr>
         <div v-if="searchList.length">
-          <ul v-for="searchChild in searchList" :key="searchChild.id">
-            <li></li>
-          </ul>
+          <ReviewMapList v-for="searchChild in searchList" :key="searchChild.id" :searchChild="searchChild"/>
+          <PaginationBar :currentPage="currentPage" :numberOfPages="numberOfPages" @click="searchStore"/>
         </div>
         <p v-else>검색결과가 없습니다.</p>
       </div>
@@ -19,33 +17,55 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import axios from 'axios'
+import ReviewMapList from '@/components/review/ReviewMapList.vue'
+import PaginationBar from '@/components/review/PaginationBar.vue'
 
 export default {
+  components: {
+    ReviewMapList,
+    PaginationBar
+  },
   setup() {
     const searchList = ref([])
     const searchWord = ref('')
-
+    const currentPage = ref(1)
+    const totalCount = ref(0)
 
     // 가게를 검색하는 함수
-    const searchStore = async() => {
+    console.log(currentPage.value)
+    const searchStore = async(page = currentPage.value) => {
       const query = searchWord.value
       try {
         const res = await axios.get(`https://dapi.kakao.com/v2/local/search/keyword.json?query=${query}`, {
           headers: {
             Authorization: `KakaoAK ${process.env.VUE_APP_KAKAO_REST_API_KEY}`
+          },
+          params: {
+            page: page,
+            size: 5
           }
         })
-        console.log(res)
+        searchList.value = res.data.documents
+        totalCount.value = res.data.meta.total_count
+        currentPage.value = page
       } catch(error) {
         console.error(error)
       }
     }
+    
+    // 페이지 수 계산
+    const numberOfPages = computed(() => {
+      return Math.ceil(totalCount.value / 5)
+    })
+
     return {
       searchWord,
       searchList,
-      searchStore
+      searchStore,
+      currentPage,
+      numberOfPages,
     }
   }
 
