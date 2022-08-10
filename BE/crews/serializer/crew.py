8cfa2969e377ae42e696392b2d18c0468ddc67a5
asdf134652 <asdf134652@gmail.com>
2022-08-10 13:20:27 +0900

    [S07P12B209-271 FIX: Crew detail

--- BE/crews/serializer/crew.py
+++ BE/crews/serializer/crew.py
@@ -23,9 +23,10 @@ class CrewListSerializer(serializers.ModelSerializer) :
 
 class CrewSerializer(serializers.ModelSerializer) :
     crew_member_count = serializers.IntegerField(source='crew_member.count', read_only=True)
+    crew_leader = serializers.CharField(source='crew_leader.nickname')
     class Meta: 
         model = Crew
-        fields= ('crew_pk','crew_name','crew_explain','crew_region','crew_img','crew_member_count','created_at')
+        fields= ('crew_pk','crew_name','crew_leader','crew_explain','crew_region','crew_img','crew_member_count','created_at')
 
 class CrewInviteListSerializer(serializers.ModelSerializer) :
     nickname = serializers.CharField(source='user.nickname', read_only=True)
