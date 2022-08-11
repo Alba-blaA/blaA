318fcb0645d8302e83d6b84bad8c102f6c7dbd14
Sohee Cheon <csh5156@gmail.com>
2022-08-11 15:03:12 +0900

    FEAT: story follow feed in progess

---
+++ FE/blaa/src/pages/story/storyFollowView.vue
@@ -0,0 +1,38 @@
+<template>
+  <StoryImageCardList :images="images"/>
+</template>
+
+<script>
+import StoryImageCardList from '@/components/story/StoryImageCardList.vue'
+import { useStore } from 'vuex'
+import { onBeforeMount, ref } from 'vue'
+
+export default {
+  components: {
+    StoryImageCardList
+  },
+  setup() {
+    const store = useStore()
+    const images = ref(null)
+
+    const getPure = () => {
+          store.dispatch('story/getFollow').then(() => {
+          images.value = store.state.story.images          
+      })
+    }
+
+    onBeforeMount(() => {
+      console.log('실행')
+      getPure()
+    })
+
+    return {
+      images
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
