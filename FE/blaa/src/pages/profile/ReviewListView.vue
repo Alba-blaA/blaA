cffba6449ac55e3e04f4c1700be25ecf4196b6ed
Juyeon <jyeon3930@naver.com>
2022-08-12 17:14:46 +0900

    [S07P12B209-281] FIX: Fix view my review, crew error

--- FE/blaa/src/pages/profile/ReviewListView.vue
+++ FE/blaa/src/pages/profile/ReviewListView.vue
@@ -3,37 +3,26 @@
   <h3><b>리뷰</b></h3>
   <hr />
 
-  <table
-    v-for="review in reviewList.results"
-    :key="review.results.reviews.review_pk"
-    style="width: 100%"
-  >
+  <table v-for="review in reviews" :key="review.review_pk" style="width: 100%">
     <tr>
-      <td rowspan="2" width="10px"></td>
-      <td rowspan="2" id="store">
-        <img id="imgStore" :src="review.reviews.store_image" />
+      <td width="10px"></td>
+      <td id="store">
+        <img id="imgStore" :src="review.store_image" />
       </td>
       <td width="20px"></td>
       <td>
-        <b>{{ review.results.reviews.store }}</b>
+        <b>{{ review.store_name }}</b>
       </td>
       <td
         rowspan="2"
         @click="
-          reviewDetail(
-            review.reviews.store_pk,
-            review.reviews.store,
-            review.reviews.review_pk
-          )
+          reviewDetail(review.store_pk, review.store_name, review.review_pk)
         "
       >
         <h3>
           <b>></b>
         </h3>
       </td>
-    </tr>
-    <tr>
-      <td>{{ review.results.reviews.star }}</td>
     </tr>
     <tr>
       <td colspan="100%"><hr /></td>
@@ -44,6 +33,7 @@
 <script>
 import { useRoute, useRouter } from "vue-router";
 import { useStore } from "vuex";
+import { ref } from "vue";
 
 export default {
   setup() {
@@ -51,7 +41,21 @@ export default {
     const router = useRouter();
     const store = useStore();
 
-    const reviewList = store.state.profile.reviewList;
+    const reviewList = store.state.profile.reviewList.results;
+    console.log("reviewList : ", reviewList);
+    console.log("reviewList length: ", reviewList.length);
+    const user_pk = ref(-1);
+    for (var i = 0; i < reviewList.length; i++) {
+      if (reviewList[i].user_pk == route.params.user_pk) {
+        user_pk.value = i;
+        console.log("i ; ", user_pk.value);
+      }
+    }
+
+    const reviews =
+      store.state.profile.reviewList.results[user_pk.value].reviews;
+    console.log("user_pk : ", user_pk.value);
+    console.log("reviewList[user_pk]", reviews);
 
     const reviewDetail = (store_pk, store_name, review_pk) => {
       router.push({
@@ -66,6 +70,8 @@ export default {
 
     return {
       reviewList,
+      user_pk,
+      reviews,
       reviewDetail,
     };
   },
