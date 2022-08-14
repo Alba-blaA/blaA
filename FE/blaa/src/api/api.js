53f0886888604ef591331e81e88bad81ec75b8e0
khlzzang <khlzzang1104@gmail.com>
2022-08-15 05:06:07 +0900

    renew

--- FE/blaa/src/api/api.js
+++ FE/blaa/src/api/api.js
@@ -12,6 +12,7 @@ const REVIEW = "reviews/";
 const STORE = "store/";
 const CREW = "crews/";
 const NOTIFICATION = "notifications/";
+const SCHEDULE = "schedule/"
 // const CHAT = 'chat/'
 
 export default {
@@ -49,6 +50,7 @@ export default {
     // myCrew: (user_pk) => HOST + CREW + user_pk + "/",
     acceptcrew: (crew_pk) => HOST + CREW + "accept_crew/" + crew_pk + "/",
     refusecrew: (crew_pk) => HOST + CREW + "deny_crew/" + crew_pk + "/",
+    registercrewschedule: (crew_pk) => HOST + CREW + SCHEDULE + crew_pk + "/"
   },
   categorys: {
     job: () => HOST + CATEGORYS + "job/",
--- FE/blaa/src/components/crew/schedule/ScheduleRegisterView.vue
+++ FE/blaa/src/components/crew/schedule/ScheduleRegisterView.vue
@@ -6,37 +6,34 @@
       <DatePicker v-model="range" mode = "dateTime" is-range  is-expanded>
         <template v-slot = "{ inputValue, inputEvents }">
           <div class="group">
-            <label class="button groupItem" for="start">Start</label>
+            <label class="button groupItem" for="start">Start &nbsp;</label>
             <input type="text" id="start" :value="inputValue.start" v-on="inputEvents.start" class="input">
             <hr>
-            <label class="button groupItem" for="end">End</label>
+            <label class="button groupItem" for="end">End &nbsp;&nbsp;</label>
             <input type="text" id="end" :value="inputValue.end" readonly class="input">  
+          </div>         
+          <hr>
+          <form style="m">  
+            <div class="form-group">
+              <select  name="color" @change="putcolor($event)" class="form-control" id="exampleFormControlSelect1">
+                <option value="">근무 선택</option>
+                <option value="grey">휴가</option>
+                <option value="red">오전 근무</option>
+                <option value="blue">오후 근무</option>
+                <option value="purple">풀타임</option>
+                <option value="teal">대타</option>
+              </select>
+            </div>                    
+          </form>
+          <br>
+          <div>
+            <button @click.prevent="showtime(inputValue.start, inputValue.end)">등록하기</button>  
+            <button @click="moveToSchedule">뒤로가기</button>
           </div>
-          <button @click.prevent="showtime(inputValue.start, inputValue.end)">등록하기</button>  
         </template>
-      </DatePicker>   
-        
+      </DatePicker>         
     </div>
 
-  <br>
-  <hr>
-  <form style="m">  
-    <div class="form-group">
-      <select  name="color" @change="putcolor($event)" class="form-control" id="exampleFormControlSelect1">
-        <option value="">근무 선택</option>
-        <option value="grey">휴가</option>
-        <option value="red">오전 근무</option>
-        <option value="blue">오후 근무</option>
-        <option value="purple">풀타임</option>
-        <option value="teal">대타</option>
-      </select>
-    </div>
-    <div class="form-group">
-      <textarea v-model = "info.content" class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="간단 메모"></textarea>
-    </div>
-   
-  </form>
-   <button @click="moveToSchedule">뒤로가기</button>
  
      
 </template>
@@ -47,6 +44,7 @@ import { useRoute, useRouter } from 'vue-router'
 import { ref, reactive } from 'vue'
 import { useStore } from "vuex";
 import axios from "@/api/axios.js"
+import api from "@/api/api.js";
 
 
 export default {
@@ -66,25 +64,60 @@ export default {
       end : date.value,     
 
     })
-
     const info = reactive({
       color : "",
       crew_pk : crew_pk,
       user_pk : userInfo.user_pk,
       content : "",
     })
-    const showtime =  ((a,b) => {
-      console.log("start : "+ a.slice(0,4) + a.slice(5));
-      console.log("end : "+ b);
-      // await axios.post(api.crew.registercrewschedule(range.crew_pk), {
-      //   crew_day : range.day,
-      //   color : range.color,
-      //   crew_starthour : range.starthour,
-      //   crew_endhour : range.endhour,
-      // }).then((response) => {
 
-      // })
+    const daytoString = ((a) => {
+      return a.slice(0,4) +"-"+ a.slice(5,7) +"-"+ a.slice(8,10)
     })
+    const timetoString = ((a) => {
+      if (a[12] == ":") {        
+        if (a.slice(16,18) == "PM") {
+          return  String(Number(a[11]) + 12)+ ":" + a.slice(13,15); 
+        } else {
+          return "0" +a[11] + ":" + a.slice(13,15);
+        } 
+              
+      } else {        
+        if (a.slice(17,19) == "PM") {
+          return String(Number(a.slice(10,13)) + 12) + ":" + a.slice(14,16);          
+        } else {
+          return  a.slice(10,13)  + ":" + a.slice(14,16);
+        }  
+      }                  
+    })
+    const showtime =  (async (a,b) => {
+      console.log("crew_day :" + daytoString(a));
+      console.log("color :" + info.color);
+      console.log("crew_starthour :" + timetoString(a));
+      console.log("crew_endhour :" + timetoString(b));
+      console.log("crew :" +  crew_pk);
+      console.log("user :" + userInfo.user_pk);
+      
+      // const data = {
+      //   crew_day : daytoString(a),
+      //   color : info.color,
+      //   crew_starthour : timetoString(a),
+      //   crew_endhour : timetoString(b),             
+      // }             
+      await axios.post(api.crew.registercrewschedule(info.crew_pk), {
+        crew_day : daytoString(a),
+        color : info.color,
+        crew_starthour : timetoString(a),
+        crew_endhour : timetoString(b),
+        crew : info.crew_pk,
+        user : userInfo.user_pk             
+      }).then((response) => {
+        console.log(response);
+
+      })
+    })
+
+
     const putcolor = ((event) => {
       info.color = event.target.value
     })
@@ -105,7 +138,9 @@ export default {
       input,
       moveToSchedule,
       info,
-      date
+      date,
+      daytoString,
+      timetoString
       // showresult
     }
   
