69ce96a074df6d3922a736a609f756be8d4c1115
Sohee Cheon <csh5156@gmail.com>
2022-08-15 15:37:30 +0900

    STYLE: Review Form Style Finish

--- FE/blaa/src/pages/review/ReviewForm.vue
+++ FE/blaa/src/pages/review/ReviewForm.vue
@@ -1,56 +1,68 @@
 <template>
   <!-- 가게 주소 -->
-  <ReviewMap v-if="isModalOpen" @close-modal="isModalOpen=false" @select-store="selectStore"/>
-  <div>
-    <router-link :to="{name: 'review'}">뒤로</router-link>
-    <button @click="sumbitReview">제출완료</button>
-  </div>
-  <h1>리뷰 작성 폼</h1>
-  <div>
-    <p class="form-input">가게명 : {{storeName}} <button class="btn btn-primary" @click="isModalOpen=true">검색</button></p>
-    <p class="form-input">가게주소 : {{storeAddress}}</p>
-    <p v-if="storeError" class="error">가게를 검색해주세요</p>
-  </div>
-  <div v-if="isStore">
-    <label for="store_picture">가게 사진 등록</label>
-    <input class="store_picture" id="store_picture" type="file" @change="previewFile"/><br />
-    <img class="img_test" src="" height="200" alt="이미지 미리보기..." />
-  </div>
-  <!-- 별점 -->
-  <span>별점 : </span>
-  <form id="myform" class="mb-3" @click="checkStar">
-    <fieldset>
-      <input type="radio" name="reviewStar" value="5" id="rate1"><label for="rate1">★</label>
-      <input type="radio" name="reviewStar" value="4" id="rate2"><label for="rate2">★</label>
-      <input type="radio" name="reviewStar" value="3" id="rate3"><label for="rate3">★</label>
-      <input type="radio" name="reviewStar" value="2" id="rate4"><label for="rate4">★</label>
-      <input type="radio" name="reviewStar" value="1" id="rate5"><label for="rate5">★</label>
-    </fieldset>
-  </form>
-  <span> {{star}}점 </span>
-  <p v-if="starError" class="error">별점을 입력해주세요</p>
-  <hr>
-  <!-- 버튼식 -->
-  <form @click="checkBtn" id="buttonReview">
-    <label for="kind" class="btn"><input type="checkbox" name="reviewBtn" vlaue="1" id="kind" class="checkList" >친절한 사장님</label>
-    <label for="clean" class="btn"><input type="checkbox" name="reviewBtn" value="2" id="clean" class="checkList">깨끗한 매장</label>
-    <label for="short" class="btn"><input type="checkbox" name="reviewBtn" value="3" id="short" class="checkList">교통 접근성</label>
-    <label for="good" class="btn"><input type="checkbox" name="reviewBtn" value="4" id="good" class="checkList">좋은 분위기</label>
-    <label for="workblance" class="btn"><input type="checkbox" name="reviewBtn" value="5" id="workblance" class="checkList">칼퇴근 가능</label>
-    <label for="uniform" class="btn"><input type="checkbox" name="reviewBtn" value="6" id="uniform" class="checkList">유니폼 제공</label>
-  </form>
+  <div class="d-flex justify-content-center">
+    <ReviewMap v-if="isModalOpen" @close-modal="isModalOpen=false" @select-store="selectStore"/>
+    <div style="width:90%">
+      <div div class="d-flex justify-content-between align-items-center" style="padding:4px;">
+        <router-link :to="{name: 'review'}" style="text-decoration:none; color:black; font-size:16px; font-weight:bold; width:60px;">X</router-link>
+        <h2 style="font-weight:bold; margin:0;">리뷰 작성 폼</h2>
+        <button class="submit-btn" @click="sumbitReview">제출</button>
+      </div>
+      <div style="height: 1px; background-color:black; margin:10px 0px;"></div>
 
-  <!-- 한줄평 -->
-  <div class="oneReview">
-    <p>한줄평</p>
-    <input type="text" class="form-input" v-model="oneReview">
-    <p v-if="oneReviewError" class="error">한줄평을 입력해주세요</p>
+      <div class="store-info" :class="{isStoreInfo: isStore, isNotStoreInfo: !isStore}">
+        <div v-if="isStore" class="store-picture">
+          <input class="store_picture" id="store_picture" type="file" @change="previewFile" style="display:none"/>
+          <img class="img_test" src="" height="60" width="60" @click="fileUpload"/>
+        </div>
+        <div>
+          <p class="form-input">가게명 : {{storeName}}</p>
+          <p class="form-input">가게주소 : {{storeAddress}}</p>
+          <p v-if="storeError" class="error">가게를 검색해주세요</p>
+        </div>
+        <button @click="isModalOpen=true">검색</button>
+      </div>
+      <!-- 별점 -->
+      <div class="d-flex justify-content-center">
+        <form id="myform" class="mb-3" @click="checkStar">
+          <fieldset>
+            <input type="radio" name="reviewStar" value="5" id="rate1"><label for="rate1">★</label>
+            <input type="radio" name="reviewStar" value="4" id="rate2"><label for="rate2">★</label>
+            <input type="radio" name="reviewStar" value="3" id="rate3"><label for="rate3">★</label>
+            <input type="radio" name="reviewStar" value="2" id="rate4"><label for="rate4">★</label>
+            <input type="radio" name="reviewStar" value="1" id="rate5"><label for="rate5">★</label>
+          </fieldset>
+        </form>
+      </div>
+      <p v-if="starError" class="error">별점을 입력해주세요</p>
+
+      <div class="button-info">
+        <!-- 버튼식 -->
+        <form @click="checkBtn" id="buttonReview">
+          <label for="kind" ><input type="checkbox" name="reviewBtn" vlaue="1" id="kind" class="checkList" >친절한 사장님</label>
+          <label for="clean" ><input type="checkbox" name="reviewBtn" value="2" id="clean" class="checkList">깨끗한 매장</label>
+          <label for="short" ><input type="checkbox" name="reviewBtn" value="3" id="short" class="checkList">교통 접근성</label>
+          <label for="good" ><input type="checkbox" name="reviewBtn" value="4" id="good" class="checkList">좋은 분위기</label>
+          <label for="workblance" ><input type="checkbox" name="reviewBtn" value="5" id="workblance" class="checkList">칼퇴근 가능</label>
+          <label for="uniform"><input type="checkbox" name="reviewBtn" value="6" id="uniform" class="checkList">유니폼 제공</label>
+        </form>
+      </div>
+      
+
+      <!-- 한줄평 -->
+      <div class="d-flex justify-content-center">
+        <div class="oneReview">
+          <p>한줄평</p>
+          <textarea v-model="oneReview" cols="30" rows="10"></textarea>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import ReviewMap from '@/components/review/ReviewMap.vue'
-import { onMounted, ref, onBeforeMount, watch, onUnmounted } from 'vue'
+import { computed, ref, onUnmounted } from 'vue'
 import $ from 'jquery'
 import { useStore } from 'vuex'
 import { useRouter } from 'vue-router'
@@ -68,25 +80,29 @@ export default {
     // 6개의 버튼으로 이루어짐
     const storeButton = ref([0,0,0,0,0,0])
     const oneReview = ref('')
-    const oneReviewError = ref(false)
     const isModalOpen = ref(false)
     const star = ref(0)
     const starError = ref(false)
-    const isStore = ref(true)
+    const isStore = ref(false)
     const store_pk = ref(0)
     const store_picture = ref(null)
     const image_url = ref('')
+    const isSubmit = ref(false)
 
     onUnmounted(() => {
       storeButton.value = [0,0,0,0,0,0]
-      console.log($('#buttonReview'))
-      console.log($('#buttonReview').find('.selected'))
-      $('#buttonReview').find('.selected').each( function() {
+      console.log($('#buttonReview').children())
+      $('#buttonReview').children().find('.selected').each( function() {
         console.log($(this))
         $(this).removeClass("selected")
       })
       console.log('언마운트')
     })
+
+    const fileUpload = (e) => {
+      e.preventDefault()
+      $('#store_picture').click()
+    }
 
     // 상점 선택하기
     const selectStore = (data) => {
@@ -156,6 +172,17 @@ export default {
         }
       })
     }
+    
+    isSubmit.value = computed(() => {
+      if (storeName.value && storeAddress.value && star.value) {
+        $('sumbit-btn').removeAttr('disabled')
+        return true
+      } else {
+        $('.submit-btn').add('disabled', 'disabled')
+        return false
+      }
+    })
+
 
     const sumbitReview = async() => {
       if ( storeName.value && storeAddress.value && star.value && oneReview.value ){
@@ -211,7 +238,6 @@ export default {
         )
       } else {
         // 에러 발생시 에러 문구 출력
-        oneReviewError.value = oneReview.value == '' ? true : false
         storeError.value = storeName.value == '' ? true : false
         starError.value = star.value == 0 ? true : false 
       }
@@ -229,15 +255,25 @@ export default {
       sumbitReview,
       starError,
       storeError,
-      oneReviewError,
       previewFile,
-      isStore
+      isStore,
+      isSubmit,
+      fileUpload
     }
   }
 }
 </script>
 
 <style scoped>
+#myform {
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  background-color: #498D6D;
+  width: 200px;
+  height: 50px;
+  border-radius: 30px;
+}
 #myform fieldset{
     display: inline-block;
     direction: rtl;
@@ -246,32 +282,125 @@ export default {
 #myform fieldset legend{
     text-align: right;
 }
-#myform > input{
+#myform fieldset label{
     cursor: pointer;
-    width:10px;
-    height: 10px;
+    width: 30px;
+    height: 30px;
+    font-size: 30px;
+    color: white
 }
+
 #myform input[type=radio]{
     display: none;
 }
+
 input[type=checkbox]{
   display: none;
 }
 .error {
   color: red;
 }
-.selected {
-  background-color:greenyellow;
+
+.submit-btn {
+  background: #498D6D;
+  border-radius: 20px;
+  border: 0;
+  width: 60px;
+  height: 35px;
 }
 
-/* 노란색이안먹어 */
+.isSubmit {
+  opacity: 1;
+}
+
+.isNotSubmit {
+  opacity: 0.5;
+}
+
+.store-info {
+  margin: 20px 0;
+  padding: 0 4px;
+  display: grid;
+}
+
+.isStoreInfo {
+  grid-template-columns: 20% 65% 15%;
+}
+
+.isNotStoreInfo {
+  grid-template-columns: 85% 15%;
+}
+
+.store-info  p {
+  height: 30px;
+  margin-bottom: 4px;
+}
+
+.store-info > button {
+  background-color: #EEC95C;
+  color: #498D6D;
+  border: 0;
+  border-radius: 20px;
+  font-weight: bold;
+  width: 80%;
+  height: 60px;
+}
+
+.store-picture {
+  margin: 0 12px;
+  border-radius: 20px;
+}
+
+.button-info {
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+}
+
+#buttonReview {
+  display: grid;
+  width: 60%;
+  background-color: #F8F9FE;
+  grid-template-columns: repeat(2, 50%);
+  row-gap: 5px;
+  column-gap: 5px;
+  padding: 20px;
+  border-radius: 20px;
+}
+
+#buttonReview label {
+  cursor: pointer;
+  padding: 10px;
+  border-radius: 10px;
+  text-align: center;
+  font-weight: bold;
+}
+
+.selected {
+  background-color:#498D6D;
+  color: white;
+}
+
+.oneReview {
+  margin-top: 25px;
+  width: 90%
+}
+
+.oneReview textarea {
+  border-radius: 20px;
+  width:100%;
+  height: 100px;
+  padding: 20px;
+}
+
+
 #myform label:hover{
-    text-shadow: 0 0 0 rgba(250, 208, 0, 0.99);
+    color:rgba(250, 208, 0, 0.99);
 }
 #myform label:hover ~ label{
-    text-shadow: 0 0 0 rgba(250, 208, 0, 0.99);
+    color:rgba(250, 208, 0, 0.99);
 }
 #myform input[type=radio]:checked ~ label{
-    text-shadow: 0 0 0 rgba(250, 208, 0, 0.99);
+    color:rgba(250, 208, 0, 0.99);
 }
 </style>
